851820ff7ccd9f7dc8750dfcd3aa48f85a8401ba
JohnGuilding <jwguilding@gmail.com>
2022-05-03 23:01:25 +0100

    Implement basic functions

--- contracts/Crowdfund.sol
+++ contracts/Crowdfund.sol
@@ -6,31 +6,45 @@ import './Multisig.sol';
 contract Crowdfund {
 
     Multisig public multisig;
+    address public multisigAddress;
     
     mapping(address => uint) public balances;
 
-    uint256 public constant threshold = 1 ether;
+    uint256 public constant threshold = 0.3 ether;
     uint256 public deadline = block.timestamp + 1 seconds;
 
     event SendFunds(address _from, uint256 _amount);
 
-    constructor(address multisigAddress) public {
-        multisig = Multisig(multisigAddress);
+    constructor(address payable _multisigAddress) public {
+        multisig = Multisig(_multisigAddress);
+        multisigAddress = _multisigAddress;
     }
 
-    function sendFunds() public payable {
-
+    function pledgeFunds() public payable {
+        balances[msg.sender] = msg.value;
+        emit SendFunds(msg.sender, msg.value);
     }
 
-    function execute() public {
+    function unpledgeFunds() public {
+        uint256 contractBalance = balances[msg.sender];
+        require(contractBalance > 0, 'contract balance must be more than zero');
 
+        balances[msg.sender] = 0;
+
+        (bool success, ) = msg.sender.call{ value: contractBalance }("");
+        require(success, 'Call failed');
     }
 
-    function timeLeft() public {
-
+    function execute() public payable {
+        require(block.timestamp >= deadline, 'Wait for the deadline');
+        uint256 contractBalance = address(this).balance;
+        if (contractBalance >= threshold) {
+            (bool success, ) = multisigAddress.call{ value: msg.value }("");
+            require(success, 'Call failed');
+        }
     }
-    
-    function withdraw() public {
 
+    function timeLeft() public view returns (uint256) {
+        return block.timestamp >= deadline ? 0 : deadline - block.timestamp;
     }
 }
--- contracts/Multisig.sol
+++ contracts/Multisig.sol
@@ -2,4 +2,9 @@
 pragma solidity ^0.8.13;
 
 contract Multisig {
+    event Deposit(address, uint256);
+    
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
 }
