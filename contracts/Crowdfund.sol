832bf9b88344bb824911076a4dee749ed23e1e93
JohnGuilding <jwguilding@gmail.com>
2022-05-04 20:39:42 +0100

    crowdfundNotCompleted & deadlinePassed modifiers

--- contracts/Crowdfund.sol
+++ contracts/Crowdfund.sol
@@ -1,50 +1,68 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import './Multisig.sol';
+import "./Multisig.sol";
 
 contract Crowdfund {
-
     Multisig public multisig;
     address public multisigAddress;
-    
-    mapping(address => uint) public balances;
+
+    mapping(address => uint256) public balances;
 
     uint256 public constant threshold = 0.3 ether;
-    uint256 public deadline = block.timestamp + 1 seconds;
+    uint256 public deadline = block.timestamp + 30 seconds;
 
     event SendFunds(address _from, uint256 _amount);
+
+    modifier deadlinePassed(bool requireDeadlinePassed) {
+        uint256 timeRemaining = timeLeft();
+        if (requireDeadlinePassed) {
+            require(timeRemaining == 0, "Deadline has not elapsed");
+            _;
+        }
+        require(timeRemaining > 0, "Deadline has passed");
+        _;
+    }
+
+    modifier crowdfundNotCompleted() {
+        require(multisig.isComplete() == false, "staking period not over");
+        _;
+    }
 
     constructor(address payable _multisigAddress) public {
         multisig = Multisig(_multisigAddress);
         multisigAddress = _multisigAddress;
     }
 
-    function pledgeFunds() public payable {
+    function pledgeFunds() public payable deadlinePassed(false) {
         balances[msg.sender] = msg.value;
         emit SendFunds(msg.sender, msg.value);
     }
 
-    function unpledgeFunds() public {
+    function unpledgeFunds() public deadlinePassed(true) crowdfundNotCompleted {
         uint256 contractBalance = balances[msg.sender];
-        require(contractBalance > 0, 'contract balance must be more than zero');
+        require(contractBalance > 0, "contract balance must be more than zero");
 
         balances[msg.sender] = 0;
 
-        (bool success, ) = msg.sender.call{ value: contractBalance }("");
-        require(success, 'Call failed');
+        (bool success, ) = msg.sender.call{value: contractBalance}("");
+        require(success, "Call failed");
     }
 
-    function execute() public payable {
-        require(block.timestamp >= deadline, 'Wait for the deadline');
+    function execute() public payable crowdfundNotCompleted {
+        require(block.timestamp >= deadline, "Wait for the deadline");
         uint256 contractBalance = address(this).balance;
         if (contractBalance >= threshold) {
-            (bool success, ) = multisigAddress.call{ value: msg.value }("");
-            require(success, 'Call failed');
+            (bool success, ) = multisigAddress.call{value: msg.value}("");
+            require(success, "Call failed");
         }
     }
 
     function timeLeft() public view returns (uint256) {
         return block.timestamp >= deadline ? 0 : deadline - block.timestamp;
+    }
+
+    receive() external payable {
+        pledgeFunds();
     }
 }
--- contracts/Multisig.sol
+++ contracts/Multisig.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.13;
 
 contract Multisig {
     event Deposit(address, uint256);
+    bool public isComplete;
+
+    function complete() public payable {
+        isComplete = true;
+    }
     
     receive() external payable {
         emit Deposit(msg.sender, msg.value);
