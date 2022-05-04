8868cc013d0acb4b7a58df125d55d097f9edf890
JohnGuilding <jwguilding@gmail.com>
2022-05-04 20:50:12 +0100

    Add comments to CrowdFund contract

--- contracts/Crowdfund.sol
+++ contracts/Crowdfund.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.13;
 
 import "./Multisig.sol";
 
+/**
+ * @title Crowdfund
+*/
 contract Crowdfund {
     Multisig public multisig;
     address public multisigAddress;
@@ -14,6 +17,12 @@ contract Crowdfund {
 
     event SendFunds(address _from, uint256 _amount);
 
+    /**
+     * @dev Can call functions marked by this modifier when 
+     * deadline has/has not passed depending on param.
+     * @param requireDeadlinePassed The bool indicating whether the 
+     * deadline should or should have not passed
+    **/
     modifier deadlinePassed(bool requireDeadlinePassed) {
         uint256 timeRemaining = timeLeft();
         if (requireDeadlinePassed) {
@@ -24,21 +33,34 @@ contract Crowdfund {
         _;
     }
 
+    /**
+     * @dev Can call functions marked by this modifier when crowdfund is not over.
+    **/
     modifier crowdfundNotCompleted() {
         require(multisig.isComplete() == false, "staking period not over");
         _;
     }
 
+    /**
+     * @dev Constructor.
+     * @param _multisigAddress The address of the multisig contract
+    */
     constructor(address payable _multisigAddress) public {
         multisig = Multisig(_multisigAddress);
         multisigAddress = _multisigAddress;
     }
 
+    /**
+     * @dev Pledge funds to the crowdfund
+    */
     function pledgeFunds() public payable deadlinePassed(false) {
         balances[msg.sender] = msg.value;
         emit SendFunds(msg.sender, msg.value);
     }
 
+    /**
+     * @dev Un-pledge funds from crowdfund
+    */
     function unpledgeFunds() public deadlinePassed(true) crowdfundNotCompleted {
         uint256 contractBalance = balances[msg.sender];
         require(contractBalance > 0, "contract balance must be more than zero");
@@ -49,6 +71,9 @@ contract Crowdfund {
         require(success, "Call failed");
     }
 
+    /**
+     * @dev Executes pledges from individuals
+    */
     function execute() public payable crowdfundNotCompleted {
         require(block.timestamp >= deadline, "Wait for the deadline");
         uint256 contractBalance = address(this).balance;
@@ -58,6 +83,9 @@ contract Crowdfund {
         }
     }
 
+    /**
+     * @dev Returns the time left before the crowdfund ends
+    */
     function timeLeft() public view returns (uint256) {
         return block.timestamp >= deadline ? 0 : deadline - block.timestamp;
     }
